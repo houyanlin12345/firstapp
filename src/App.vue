a89ca795f752be73a718c6b6f220f8c6252302fb
Your Name <you@example.com>
2020-12-08 11:05:44 +0800

    add png

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <img src="./assets/homepage.png" />
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app img {
+  width: 100%;
+}
+</style>
